abd7a4d9809b461e914d78215e2b5654170dd3a4
Stijn van Lierop (DBS) <stijn@hkqd5x3-l.holmes.nl>
2024-05-31 13:14:19 +0200

    added docstrings to utils function

--- creation/utils.py
+++ creation/utils.py
@@ -2,8 +2,14 @@ import cv2
 from PIL import Image
 import numpy as np
 
-def complexity(pic):
-    lab_img = cv2.cvtColor(pic, cv2.COLOR_RGB2LAB)
+
+def complexity(img: np.ndarray) -> float:
+    """
+    Calculates the complexity of an image as defined by:
+    :param img: numpy array containing the image data in opencv format.
+    :return: the complexity of the image.
+    """
+    lab_img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
 
     M = lab_img.shape[0]
     N = lab_img.shape[1]
@@ -23,14 +29,32 @@ def complexity(pic):
 
     return complexity
 
-def PIL_to_opencv(image_path):
-	pil_image = Image.open(image_path).convert('RGB')
-	return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
-def average_hue(img):
+def PIL_to_opencv(image_path: str) -> np.ndarray:
+    """
+    Opens an image using PIL and converts it to an opencv numpy array.
+    :param image_path: path of the image.
+    :return: numpy array containing the image data in opencv format.
+    """
+    pil_image = Image.open(image_path).convert('RGB')
+    return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+
+
+def average_hue(img: np.ndarray) -> float:
+    """
+    Calculates the average hue of the given image.
+    :param img: numpy array containing the image data in opencv format.
+    :return: The average hue of the image.
+    """
     hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    avg_colour = np.mean(hsv_img[:,:,0])
+    avg_colour = np.mean(hsv_img[:, :, 0])
     return avg_colour
 
-def motion_blur(img):
+
+def motion_blur(img: np.ndarray) -> float:
+    """
+    Calculates the motion blur of the given image.
+    :param img: numpy array containing the image data in opencv format.
+    :return: The motion blue of the image.
+    """
     return cv2.Laplacian(img, cv2.CV_64F).var()
